2de3f2e1e7a939f4b08b41d6eea5158c41ab1559
Ryan <49543952+ryanccf@users.noreply.github.com>
2021-11-05 08:20:08 -0500

    Add Shooting
    Bullets now get shot upon pressing a fire button (click or space)

--- Bullet.gd
+++ Bullet.gd
@@ -1,10 +1,22 @@
 extends Node2D
 
+var SPEED = 200
+var velocity = 0
+
 func _ready():
-	print(get_parent().name)
-	global_position = get_parent().global_position
 	set_as_toplevel(true)
-	position = get_parent().get_position()
+	position = get_parent().barrel_position()
+	rotation = get_parent().get_rotation()
+	velocity = Vector2(1, 0).rotated(rotation) * SPEED
+	print(velocity)
+
+func _physics_process(delta):
+	$KinematicBody2D.move_and_collide(velocity * delta)
 
 func _on_VisibilityNotifier2D_screen_exited():
+	print("EXITED SCREEEN")
+	print(position)
+	$DeathTimer.start()
+
+func _on_DeathTimer_timeout():
 	queue_free()
--- Gun.gd
+++ Gun.gd
@@ -11,6 +11,7 @@ func move_hand(delta):
 	gun_angle += delta
 	rotation = (sin(gun_angle) * 0.85) + 30
 	position.y = (sin(hand_shakiness * 2) * 6) + 120
+	position.x = (sin(hand_shakiness) * 4) + 20
 
 func _process(delta):
 	move_hand(delta)
@@ -19,6 +20,9 @@ func _process(delta):
 func accept_input():
 	if Input.is_action_pressed("fire"):
 		add_child(load("res://Bullet.tscn").instance())
+
+func barrel_position():
+	return $Position2D.global_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
