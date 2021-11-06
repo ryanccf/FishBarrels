8c2df4a9c6d6104961dfe2f2fcd6aa9006bcfc63
Ryan <49543952+ryanccf@users.noreply.github.com>
2021-11-06 08:51:23 -0500

    Flip Foosh
    The fish must be flipped when they hit the wall.

--- Fish.gd
+++ Fish.gd
@@ -1,33 +1,46 @@
 extends Node2D
-const SWIM_MAX = 300
+
+var rng = RandomNumberGenerator.new()
+
+var swim_max = 300
 var swimtime = 0
 var facing = -1
+#var FLIP_X
 
 func _ready():
-	pass
+	rng.randomize()
+	swim_max = rng.randf_range(20, 300)
 
 func _process(delta):
 	position.x += delta * 10 * facing
 	check_swimtime()
-	#check_facing()
 
 func check_swimtime():
-	if swimtime < SWIM_MAX:
+	if swimtime < swim_max:
 		swimtime += 1
 	else:
 		toggle_facing()
 		swimtime = 0
 	
 func check_facing():
+	var last_position = position
 	if facing == -1:
-		position *= Vector2(-1, 0)
-		pass
+		position = last_position
 	else:
-		pass
-		position *= Vector2(-1, 1)
+		Transform2D.affine_inverse()
 
 func toggle_facing():
 	facing *= -1
+	if facing == 1:
+		var FLIP_X = Transform2D(Vector2(-1, 0), Vector2(0, 1), Vector2(0, 0))
+		var last_position = position
+		transform = FLIP_X
+		position = last_position
+	else:
+		var FLIP_X = Transform2D(Vector2(1, 0), Vector2(0, 1), Vector2(0, 0))
+		var last_position = position
+		transform = FLIP_X
+		position = last_position
 
 func _on_Area2D_area_entered(area):
 	toggle_facing()
