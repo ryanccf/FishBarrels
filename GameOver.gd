ce53be282e37a1f2b1b96d933a5158fac79f3c46
Ryan <49543952+ryanccf@users.noreply.github.com>
2021-11-17 09:26:07 -0600

    Reload Game
    Implement the loading of a new game from the Game Over scene using the new methodology.
    Note to self: don't recursively preload scenes.

--- GameOver.gd
+++ GameOver.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
-var game = "res://Game.tscn"
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Background/numfish.set_text(str(global.get_fish()))
 
 func _process(_delta):
 	if Input.is_action_pressed("fire"):
-		get_tree().change_scene(game)
+		load_new_game()
+		#get_tree().change_scene(game)
+
+func load_new_game():
+	var daddy = get_parent()
+	daddy.remove_child(self)
+	daddy.add_child(load("res://Game.tscn").instance())
+	call_deferred("free")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
