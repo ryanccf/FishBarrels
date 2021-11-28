9eb0f412dfb69d72ff2dc24abbc68bd5019eeb4e
Ryan <49543952+ryanccf@users.noreply.github.com>
2021-11-28 09:02:19 -0600

    Made fish bob up and down.
    Fish now bob up and down according to a sine wave with random amplitude and frequency generated upon fish spawn. To keep things not-glitchy in this brave new world of vertical fish movement we made fish no longer change direction when bumping into each other or the bottom of the tank. Separating the bottom and sides of the tank affected Bullet's collision code. Barrel got a slight optimization of simply not generating random seeds too many times.

--- Fish.gd
+++ Fish.gd
@@ -5,27 +5,44 @@ const SWIM_MAX_FLOOR = 2
 const SWIM_MAX_CEILING = 8
 const SWIM_SPEED_FLOOR = 800
 const SWIM_SPEED_CEILING = 1200
+const BOBBING_AMPLITUDE_FLOOR = 1
+const BOBBING_AMPLITUDE_CEILING = 8
+const BOBBING_FREQUENCY_FLOOR = 1
+const BOBBING_FREQUENCY_CEILING = 8 
 
 var rng = RandomNumberGenerator.new()
 var swim_max
 var swimtime = 0
 var facing = 1
 var motion = Vector2(0,0)
+var running_delta = 0
+var bobbing_amplitude
+var bobbing_frequency
 
 func _ready():
 	rng.randomize()
 	swim_max = rng.randf_range(SWIM_MAX_FLOOR, SWIM_MAX_CEILING)
 	motion.x = rng.randf_range(SWIM_SPEED_FLOOR, SWIM_SPEED_CEILING)
+	bobbing_amplitude = rng.randf_range(BOBBING_AMPLITUDE_FLOOR, BOBBING_AMPLITUDE_CEILING)
+	bobbing_frequency = rng.randf_range(BOBBING_FREQUENCY_FLOOR, BOBBING_FREQUENCY_CEILING)
 	if rng.randi_range(0, 1):
 		toggle_facing()
 
 func swim_and_collide(delta):
-	move_and_slide(Vector2(motion.x * delta * facing * -1, 0))
-	return get_slide_count() > 0
+	running_delta = fmod(running_delta + delta, 2*PI)
+	var x_motion = motion.x * delta * facing * -1
+	var y_motion = sin(running_delta * bobbing_frequency) * bobbing_amplitude
+	move_and_slide(Vector2(x_motion, y_motion))	
+	for collision_index in range(get_slide_count()):
+		var collision_name = get_slide_collision(collision_index).collider.name
+		if collision_name == "BarrelSides":
+			return true
 
 func update_swimtime(delta):
-	swimtime = fmod(swimtime + delta, swim_max)
-	return swimtime > swim_max
+	swimtime += delta
+	var status = swimtime > swim_max
+	swimtime = fmod(swimtime, swim_max)
+	return status
 	
 func _physics_process(delta):
 	if swim_and_collide(delta) or update_swimtime(delta):
