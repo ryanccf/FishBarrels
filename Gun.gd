d9578b9f2b847b52258e91e0cf10524d78681a74
Ryan <49543952+ryanccf@users.noreply.github.com>
2021-12-06 07:46:08 -0600

    Add Gun Sounds
    Add a shooting sound when the player fires the gun.

--- Gun.gd
+++ Gun.gd
@@ -3,6 +3,7 @@ extends Node2D
 var gun_angle = 0
 var hand_shakiness = 0
 var ready_to_fire = true
+var gun_shot = preload("res://synth_misc_10.ogg")
 
 func move_hand(delta):
 	hand_shakiness += delta * 1.123
@@ -15,13 +16,18 @@ func _ready():
 	move_hand(0)
 
 func _process(delta):
+	#print($AudioStreamPlayer2D.get_playback_position())
 	move_hand(delta)
 	accept_input()
 
 func accept_input():
 	if Input.is_action_just_pressed("fire"):
 		if ready_to_fire:
+			play_sound()
 			fire()
+
+func play_sound():
+	$AudioStreamPlayer.play()
 
 func fire():
 	add_child(load("res://Bullet.tscn").instance())
